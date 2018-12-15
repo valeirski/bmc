dc95002cb24715a57a3a860e955ca59553774f2d
valeirski <info@vbworks.net>
2018-12-15 22:55:10 +0100

    initial commit

---
+++ contact.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <meta http-equiv="refresh" content="3;url=http://www.bmc-betonwerken.be/" />
+    </head>
+    <body>
+      <?php
+          ini_set( 'display_errors', 1 );
+          error_reporting( E_ALL );
+          $from = $_POST["email"];
+          $to = "mangelschotsroel@hotmail.com";
+          $subject = "contact formulier bmc betonwerken " . $_POST["name"];
+          $message = $_POST["address"] . "\r\n" . $_POST["message"];
+          $headers = "From:" . $from;
+          if(mail($to,$subject,$message, $headers)) {
+            echo "<h1>Uw bericht werd verzonden.<h1>";
+          } else {
+            echo "Verzenden van bericht mislukt.";
+          }
+      ?>
+    </body>
+</html>
